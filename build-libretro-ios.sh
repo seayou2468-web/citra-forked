55c3af2edd1579a739a926e400248f1af28ddac9
seayou2468-web <257717896+seayou2468-web@users.noreply.github.com>
2026-02-13 08:29:37 +0000

    Update CMake and workflow for JIT-less iOS Libretro build

--- build-libretro-ios.sh
+++ build-libretro-ios.sh
@@ -12,12 +12,18 @@ cmake -GNinja \
     -DCMAKE_XCODE_ATTRIBUTE_CODE_SIGNING_ALLOWED=NO \
     -DCMAKE_XCODE_ATTRIBUTE_CODE_SIGN_IDENTITY="" \
     -DCMAKE_POLICY_VERSION_MINIMUM=3.5 \
+    -DENABLE_LIBRETRO=ON \
+    -DCITRA_FORCE_INTERPRETER=ON \
     -DENABLE_TESTS=OFF -DENABLE_DEDICATED_ROOM=OFF \
     -DENABLE_SDL2=OFF -DENABLE_QT=OFF -DENABLE_WEB_SERVICE=OFF -DENABLE_SCRIPTING=OFF \
     -DENABLE_OPENAL=OFF -DENABLE_LIBUSB=OFF -DCITRA_ENABLE_BUNDLE_TARGET=OFF \
     ..
 ninja -j$(sysctl -n hw.ncpu)
 
-SO=citra_libretro.dylib
+SO=citra_libretro_ios.dylib
+if [ ! -f "$SO" ]; then
+    echo "Error: $SO not found. Checking if it was named differently..."
+    ls -l *.dylib
+fi
 echo "Core file is here => $(readlink -f $SO)"
 strip -S $SO
